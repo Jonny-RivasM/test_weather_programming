48350cf5e3c75cde5f42ed6aae4065eadcb3b095
Jonny Rivas <jonrimo@gmail.com>
2021-10-03 12:13:50 -0500

    first commit

---
+++ ApiProgrammingTest/Controllers/WeatherController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProgrammingTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+    }
+}
